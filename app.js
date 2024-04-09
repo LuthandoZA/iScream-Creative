a82da39031016c546d230b48369f44a82aaa1771
LuthandoZA <mtengwaneluthando@gmail.com>
2024-04-09 15:44:40 +0200

    Navbar Code

--- app.js
+++ app.js
@@ -1,1 +1,56 @@
-console.log("Hello world!");
+document.addEventListener("DOMContentLoaded", function() {
+    let tl = gsap.timeline({paused: true});
+
+    tl.to(".menu-overlay", {
+        duration: 1,
+        clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
+        ease: "power2.out"
+    });
+
+    tl.from(
+        ".menu-link, .btn",
+        {
+            opacity: 0,
+            y: 60,
+            stagger: 0.05,
+            duration: 0.75,
+            ease: "power1.inOut",
+        },
+        "<",
+    );
+
+    tl.to(
+        ".video-preview",
+        {
+            duration: 1,
+            height: "200px",
+            ease: "power2.out",
+        },
+        "<",
+    );
+
+    tl.to(
+        ".menu-divider",
+        {
+            duration: 2,
+            width: "100%",
+            ease: "power4.out",
+        },
+        "<",
+    );
+
+    function openMenu() {
+        document.querySelectorAll(".menu-overlay").style.pointerEvents = "all";
+        tl.play();
+    }
+
+    function closeMenu() {
+        document.querySelectorAll(".menu-overlay").style.pointerEvents = "none";
+        tl.reverse();
+    }
+
+    document.querySelectorAll(".menu-btn-open").addEventListener("click", openMenu);
+    document.querySelectorAll(".menu-btn-close").addEvent("click", closeMenu);
+    tl.reverse();
+
+});
